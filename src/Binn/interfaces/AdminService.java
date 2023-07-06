89383035872019b335519ab630c78776b79892b5
Amalwin <amalwin.stanly@gmail.com>
2023-07-06 15:26:04 +0530

    Updated

--- src/Binn/interfaces/AdminService.java
+++
@@ -1,6 +0,0 @@
-//private void removePoint(){
-//        displayRoute();
-//        System.out.println("Enter the index of the point to be removed: ");
-//        int index = Validator.getNumberInRange(adminService.viewRoute().size());
-//        System.out.println(adminService.removePoints(index-1));
-//        }
